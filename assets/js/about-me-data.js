69511f1fdc654f3149a011d9b83bc0811d1a82ea
Rafa <rafikax12@gmail.com>
2024-02-02 19:54:48 +0100

    añadidos cambios en el script del index para manejar los cambios de contenido en la seccion about me al clicar los botones

---
+++ assets/js/about-me-data.js
@@ -0,0 +1,22 @@
+const contentData = {
+    1: {
+        header: "Header 1",
+        paragraph1: "Paragraph 1 for face 1",
+        paragraph2: "Additional paragraph for face 1"
+    },
+    2: {
+        header: "Header 2",
+        paragraph1: "Paragraph 1 for face 2",
+        paragraph2: "Additional paragraph for face 2"
+    },
+    3: {
+        header: "Header 3",
+        paragraph1: "Paragraph 1 for face 3",
+        paragraph2: "Additional paragraph for face 3"
+    },
+    4: {
+        header: "Header 4",
+        paragraph1: "Paragraph 1 for face 4",
+        paragraph2: "Additional paragraph for face 4"
+    }
+};
